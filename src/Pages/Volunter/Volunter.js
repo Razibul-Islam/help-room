a8135ed2f77de3d39eb188efad8087625da35c60
Razibul Islam <razibulislam665@gmail.com>
2023-01-31 17:13:40 +0600

    my added

--- src/Pages/Volunter/Volunter.js
+++ src/Pages/Volunter/Volunter.js
@@ -4,7 +4,7 @@ import img from '../../img/carousel1.webp';
 
 const Volunter = () => {
     return (
-        <div className='my-4 text-center'>
+        <div className='my-10 text-center'>
             <h3 className='font-bold mb-4 fill-stone-500 text-3xl text-center'>MEET OUR VOLUNTEERS</h3>
             <div className='grid md:grid-cols-3 mb-8 gap-4'>
                 <div>
